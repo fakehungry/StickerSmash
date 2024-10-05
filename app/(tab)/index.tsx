0b14fa6575e29d9d3184f36ac52afbcbb351d300
fakehungry <chaikitr@gmail.com>
2024-10-05 20:01:33 +0700

    feat - tab navigator

---
+++ app/(tab)/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "#61dafb",
+    borderRadius: 5,
+    marginTop: 20,
+  },
+});
